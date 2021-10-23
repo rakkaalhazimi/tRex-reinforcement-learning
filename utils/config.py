40fdb6787487969000ac8b112d347aece70931b8
unknown <rakkakeren@gmail.com>
2021-10-23 19:14:48 +0700

    increase max rewards

--- utils/config.py
+++ utils/config.py
@@ -4,14 +4,14 @@ import numpy as np
 SERVER = "http://localhost:5500/"
 SEED = 42
 TRAIN = True
-CONTINUE = False
-MAX_REWARDS = 5e+4
+CONTINUE = True
+MAX_REWARDS = 5e+5
 
 # Training parameter
 NUM_ACTS = 3
 UNITS = 128
 PARAM_NUM = 6
-EPISODES = 500
+EPISODES = 187
 LEARNING_RATE = 1e-2
 GAMMA = 0.99
 EPS = np.finfo(np.float32).eps.item()
